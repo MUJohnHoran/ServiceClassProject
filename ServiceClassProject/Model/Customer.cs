861f258dfee252f6e5c5537c848f381ee5ee6de5
John Horan <harrison.horan@marquette.edu>
2023-11-30 13:12:48 -0600

    Created Customer class.

---
+++ ServiceClassProject/Model/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceClassProject.Model;
+
+public class Customer
+{
+
+    public int id {  get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string UserName {  get; set; }
+    public string Email { get; set; }
+
+
+
+}
